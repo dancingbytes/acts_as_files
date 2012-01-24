7c6d85736385fad2403015b1714f828a15ad651d
Tyralion <piliaiev@gmail.com>
2012-01-24 09:52:53 +0600

    Плагин неверно подмешивался в ActiveRecord.

--- lib/acts_as_files/active_record/base.rb
+++ lib/acts_as_files/active_record/base.rb
@@ -7,16 +7,16 @@ module ActsAsFiles
   module Base
     
     def acts_as_files(&block)
-      
+        
       r = ActsAsFiles::Builder::Context.new
       r.instance_eval &block if block_given?
-      
+        
       ActsAsFiles::Manager.new(self, r.compile)
-      
+        
     end # act_as_files
     
   end # Base
   
 end # ActsAsFiles
 
-ActiveRecord::Base.send(:include, ActsAsFiles::Base)
+ActiveRecord::Base.send(:extend, ActsAsFiles::Base)
